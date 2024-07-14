ad60e697b157c6bb9ac2f856c1dd17e1897a884c
himel <2005107@cse.buet.ac.bd>
2024-07-14 21:12:27 +0600

    "decision tree done"

--- Decision Tress/DecisionTree-Classification.py
+++ Decision Tress/DecisionTree-Classification.py
@@ -10,8 +10,9 @@ class Node:
         self.values= values
         self.is_leaf= is_leaf
 
-class DecisionTreeClassifier:
-    def __init__(self,min_sample_split = 2,max_depth=2):
+class Decision_Tree_Classifier:
+    def __init__(self,min_sample_split = 2,max_depth=2,default_citeria='entropy'):
+        self.default_citeria= default_citeria
         self.min_sample_split= min_sample_split
         self.max_depth= max_depth
         self.root= None
@@ -24,10 +25,18 @@ class DecisionTreeClassifier:
         ps= hist/len(y)
         return -np.sum([p*np.log2(p) for p in ps if p>0])
     
+    def giniscore(self,y):
+        hist= np.bincount(y)
+        ps= hist/len(y)
+        return 1- np.sum([p**2 for p in ps])
     
-    def information_gain(self,left_y, right_y, current_entropy):
+
+    def information_gain(self,left_y, right_y, current_entropy,default_citeria='entropy'):
         p= float(len(left_y))/(len(left_y)+len(right_y))
-        return current_entropy- p*self.entropy(left_y)-(1-p)*self.entropy(right_y)
+        if default_citeria=='entropy':
+            return current_entropy- p*self.entropy(left_y)-(1-p)*self.entropy(right_y)
+        else:
+            return current_entropy- p*self.giniscore(left_y)-(1-p)*self.giniscore(right_y)
     
     def split(self,X, y, feature_index, threshold):
         left_indices= np.where(X[:,feature_index]<=threshold)
@@ -55,8 +64,12 @@ class DecisionTreeClassifier:
             averages = [(unique_classes[i] + unique_classes[i + 1]) / 2 for i in range(x)]
             for j in averages:
                 y_left, y_right= self.split_y(X, y, m, j)
-                entropy_parent= self.entropy(y)
-                M=self.information_gain(y_left, y_right, entropy_parent)
+                if(self.default_citeria=='entropy'):
+                    entropy_parent= self.entropy(y)
+                    M=self.information_gain(y_left, y_right, entropy_parent)
+                else:
+                    entropy_parent= self.giniscore(y)
+                    M=self.information_gain(y_left, y_right, entropy_parent,default_citeria='gini')
                 if M>max_information_gain:
                     max_information_gain=M
                     best_feature_index= m
@@ -68,7 +81,7 @@ class DecisionTreeClassifier:
 
 
     
-    def buildTreeRecursive(self,dataset_x,dataset_y,depth=0):
+    def buildTreeRecursive(self,dataset_x,dataset_y,depth=1):
         num_samples= len(dataset_y)
         num_features= dataset_x.shape[1]
         #print(num_samples,num_features)
@@ -91,6 +104,19 @@ class DecisionTreeClassifier:
             print('Tree is empty')
         else:
             self.print_tree_recursive(self.root)
+    def predict(self,X):
+        return np.array([self.predict_recursive(x,self.root) for x in X])
+    
+    def predict_recursive(self,x,node):
+        if node.is_leaf:
+            return np.argmax(node.values)
+        if x[node.feature_index]<=node.feature_threshold:
+            return self.predict_recursive(x,node.left_child)
+        return self.predict_recursive(x,node.right_child)
+    
+    def accuracy_score(self,y_true,y_pred):
+        return np.sum(y_true==y_pred)/len(y_true)
+    
     
     def print_tree_recursive(self,node):
         if node.is_leaf:
@@ -111,6 +137,35 @@ class DecisionTreeClassifier:
             print('------------------------------')
             print('right child')
             self.print_tree_recursive(node.right_child)
+
+#? Testing the model
+#! from sklearn.datasets import load_iris
+#! from sklearn.model_selection import train_test_split
+#! from sklearn.metrics import accuracy_score
+#! from sklearn.tree import DecisionTreeClassifier
+#! data= load_iris()
+#! X= data.data
+#! y= data.target
+#! X_train, X_test, y_train, y_test= train_test_split(X,y,test_size=0.5,random_state=42)
+#! model= DecisionTreeClassifier(max_depth=5,min_samples_split=2)
+#! model.fit(X_train,y_train)
+#! y_pred= model.predict(X_test)
+#! print('Accuracy of sklearn model:',accuracy_score(y_test,y_pred))
+
+
+#? lets try our decision tree
+#! X_train_my_model= X_train
+#! y_train_my_model= y_train.reshape(-1,1).flatten()
+#! X_test_my_model= X_test
+#! y_test_my_model= y_test.reshape(-1,1).flatten()
+#! model= Decision_Tree_Classifier(min_sample_split=2,max_depth=5,default_citeria='gini')
+#! model.build_tree(X_train_my_model,y_train_my_model)
+#! y_pred_2= model.predict(X_test_my_model)
+#! print('Accuracy of my model:',model.accuracy_score(y_test_my_model,y_pred_2))
+#? accuracy of both models are same which is 0.92 for entropy and 0.946 and .933 for gini
+#? so our model is working fine
+#? we have successfully copied the sklearn decision tree model
+
 
    
 
